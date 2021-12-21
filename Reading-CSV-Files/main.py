da4efe7d02d9a52212b8e40f43ae3aa6f3f167d4
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-21 14:53:26 -0500

    Update main.py

--- Reading-CSV-Files/main.py
+++ Reading-CSV-Files/main.py
@@ -5,21 +5,21 @@
 # The last way requires the installation of the Pandas package
 
 # First Way
-# open the file "data.csv" and put the data in a list
-# with open("./data.csv") as csv_file:
-#     csv_data = csv_file.readlines()
+open the file "data.csv" and put the data in a list
+with open("./data.csv") as csv_file:
+    csv_data = csv_file.readlines()
 
 # Second Way
 # import csv
-# with open("./data.csv") as csv_file:
-#     csv_data = csv.reader(csv_file)
-#     temperatures = []
-#     for row in csv_data:
-#         if row[1] == 'temp':
-#             continue
-#         temperatures.append(int(row[1]))
-#
-# print(temperatures)
+with open("./data.csv") as csv_file:
+    csv_data = csv.reader(csv_file)
+    temperatures = []
+    for row in csv_data:
+        if row[1] == 'temp':
+            continue
+        temperatures.append(int(row[1]))
+
+print(temperatures)
 
 # Third Way
 import pandas
