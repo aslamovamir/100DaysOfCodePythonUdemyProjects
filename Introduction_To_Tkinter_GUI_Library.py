79bda6ab0bb943d2c1d98dd39e887df7a8cef3b3
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-21 20:30:02 -0500

    Update Introduction_To_Tkinter_GUI_Library.py

--- Introduction_To_Tkinter_GUI_Library.py
+++ Introduction_To_Tkinter_GUI_Library.py
@@ -12,10 +12,44 @@ window.minsize(width=500, height=300)
 
 # create a label from a Label class from the tkinter library
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+
 # place the label onto the window with the "pack" method
 # we can also specify where to pack the label with the parameter "side": options-bottom,left,right,top
 # we can also make it "expand" onto the window
-my_label.pack(side="left")
+# my_label.pack(side="left")
+my_label.pack()
+
+# configure the properties of a component
+# 1st way
+my_label["text"] = "New Text"
+# 2nd way
+my_label.config(text="New Text")
+
+
+# event listener for the button
+def button_clicked():
+    print("I got clicked")
+    # also change the label text content
+    # 1st way
+    # my_label["text"] = "The button got clicked!"
+    # 2nd way
+    my_label.config(text=input.get())
+
+
+# Button
+# set the event listener to trigger the "button_clicked" function
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+# set the width of the entry component
+input = tkinter.Entry(width=10)
+# pack it onto the window
+input.pack()
+# get hold of the user input
+print(input.get())
+# when the button is clicked the text content of the label will change
+# to the input of the user
 
 # to keep the window object up on the screen, we need a loop
 # we will use the method that comes with the tkinter library, "mainloop()"
